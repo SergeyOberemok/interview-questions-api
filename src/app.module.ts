141161ecad1905a000f6a44f3a842c44f89dae5a
SergeyOberemok <sergey.oberemok@gmail.com>
2024-07-24 18:17:25 +0200

    [feat] Added config service and env file. Updated package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,9 +8,23 @@ import { LabelsModule } from './labels/labels.module';
 import { QuestionsModule } from './questions/questions.module';
 import { UsersModule } from './users/users.module';
 
+console.log(process.env.NODE_ENV, process.env.NODE_ENV ? '.dev.env' : '.env');
+
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/interview-questions'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const host = configService.get<string>('DATABASE_HOST');
+        const db = configService.get<string>('DATABASE_DB');
+
+        return { uri: `mongodb://${host}/${db}` };
+      },
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      envFilePath: ['.dev.env'],
+    }),
     AuthModule,
     UsersModule,
     QuestionsModule,
