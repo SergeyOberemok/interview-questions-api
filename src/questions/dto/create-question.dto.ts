b33c43fa2a3ea1e503d123d7aff898b954c13b23
SergeyOberemok <sergey.oberemok@gmail.com>
2024-08-06 12:24:41 +0200

    [fix] Updated answer model with lang type

--- src/questions/dto/create-question.dto.ts
+++ src/questions/dto/create-question.dto.ts
@@ -1,8 +1,8 @@
-import { IQuestion } from '../schema/question.schema';
+import { IAnswer, IQuestion } from '../schema/question.schema';
 
 export class CreateQuestionDto implements IQuestion {
   description: string;
-  answers: string[];
+  answers: IAnswer[];
   notes: string;
   labels: string[];
 }
